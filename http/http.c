d39fdb7fad26a6c66ba1e46738e4fab377f7705b
vr009 <slavarianov@yandex.ru>
2022-03-18 11:30:00 +0300

    cmake fix

--- http/http.c
+++ http/http.c
@@ -320,7 +320,7 @@ void send_response(int sock_d, http_request* req, http_response * resp, struct c
 		define_content_type(req, resp);
 		send_headers(sock_d, resp);
 #ifdef __linux__
-		snedfile(sock_d, fd, 0, statistics.st_size);
+		sendfile(sock_d, fd, 0, statistics.st_size);
 #elif __APPLE__
 		sendfile(sock_d, fd, 0, &statistics.st_size, NULL,  0);
 #endif
