deb02932217698ac91a238a63d2772eb6880af75
vr009 <slavarianov@yandex.ru>
2022-03-18 16:25:05 +0300

    build script updated

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+git submodule init &&
+git submodule update --recursive &&
+git submodule add https://github.com/mksdev/libev-cmake &&
 cd libev-cmake && rm .gitmodules &&
 git submodule add https://github.com/enki/libev &&
 cd .. &&
