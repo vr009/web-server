fe99fe499f5d374b2bb44019a7d58518e413e0ca
vr009 <slavarianov@yandex.ru>
2022-03-18 16:11:02 +0300

    build script added

---
+++ build.sh
@@ -0,0 +1,6 @@
+git submodule init
+git submodule add https://github.com/mksdev/libev-cmake
+cd libev-cmake
+git submodule add https://github.com/enki/libev
+cd ..
+mkdir build && cd build && cmake .. && make
