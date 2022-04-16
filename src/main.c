4d5b8dcf0661079cc4fd47d452bdf6bfe45007e0
vr009 <slavarianov@yandex.ru>
2022-04-16 19:40:10 +0300

    threads added

--- src/main.c
+++ src/main.c
@@ -55,7 +55,7 @@ static int create_serverfd(char const *addr, uint16_t u16port)
 }
 
 const int MAX_THREADS_WORKERS = 5;
-pthread_t threads_in_work[MAX_THREADS_WORKERS];
+static pthread_t threads_in_work[5];
 int threads_arr_size = 0;
 
 void join_all_threads() {
